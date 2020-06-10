2c4bda59dbd2bfbdf4bad71f73e1cf6dc5427074
Mcginlay <amcginla@38f9d3533231.ant.amazon.com>
2020-06-10 15:44:35 +0100

    update deployment for lamba

--- extract-and-deploy.py
+++ extract-and-deploy.py
@@ -1,19 +1,25 @@
 import boto3
+import json
 import zipfile
 from io import BytesIO
 import io
 import mimetypes
 
-s3 = boto3.resource('s3')
-
-src = s3.Bucket('build.awsome.education')
-dest = s3.Bucket('awsome.education')
-
-zip = io.BytesIO()
-src.download_fileobj('build.zip', zip)
-
-with zipfile.ZipFile(zip) as myzip:
-  for filename in myzip.namelist():
-    fileobj = myzip.open(filename)
-    dest.upload_fileobj(fileobj, filename, ExtraArgs={'ContentType': mimetypes.guess_type(filename)[0]})
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3')
     
+    src = s3.Bucket('build.awsome.education')
+    dest = s3.Bucket('awsome.education')
+    
+    zip = io.BytesIO()
+    src.download_fileobj('build.zip', zip)
+    
+    with zipfile.ZipFile(zip) as myzip:
+      for filename in myzip.namelist():
+        fileobj = myzip.open(filename)
+        dest.upload_fileobj(fileobj, filename, ExtraArgs={'ContentType': mimetypes.guess_type(filename)[0]})
+        
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
