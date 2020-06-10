479ff8799c832ba52b884c5909ad1bf46046eaf5
Mcginlay <amcginla@38f9d3533231.ant.amazon.com>
2020-06-10 16:26:25 +0100

    use SNS notifications

--- extract-and-deploy.py
+++ extract-and-deploy.py
@@ -7,19 +7,24 @@ import mimetypes
 
 def lambda_handler(event, context):
     s3 = boto3.resource('s3')
-    
-    src = s3.Bucket('build.awsome.education')
-    dest = s3.Bucket('awsome.education')
-    
-    zip = io.BytesIO()
-    src.download_fileobj('build.zip', zip)
-    
-    with zipfile.ZipFile(zip) as myzip:
-      for filename in myzip.namelist():
-        fileobj = myzip.open(filename)
-        dest.upload_fileobj(fileobj, filename, ExtraArgs={'ContentType': mimetypes.guess_type(filename)[0]})
-        
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+    sns = boto3.resource('sns')
+
+    try:
+      src = s3.Bucket('build.awsome.education')
+      dest = s3.Bucket('awsome.education')
+      topic = sns.Topic('arn:aws:sns:us-east-1:390758498079:DeployedToProduction')
+      
+      zip = io.BytesIO()
+      src.download_fileobj('build.zip', zip)
+      
+      with zipfile.ZipFile(zip) as myzip:
+        for filename in myzip.namelist():
+          fileobj = myzip.open(filename)
+          dest.upload_fileobj(fileobj, filename, ExtraArgs={'ContentType': mimetypes.guess_type(filename)[0]})
+
+      print('Job done!')
+      topic.publish(Subject='extract-and-deploy SUCCESSFUL', Message='Successful deployment to ' + dest.name)
+
+    except:
+      topic.publish(Subject='extract-and-deploy FAILED', Message='Failed deployment to ' + dest.name)
+      raise
